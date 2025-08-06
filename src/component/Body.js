2b60c42182dcd44256a5f7452dfc7b122cf77584
SachinSinghcse <116021882+SachinSinghcse@users.noreply.github.com>
2025-08-06 23:39:35 +0530

    changes in api for card and fix cors error

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,9 +1,27 @@
 import RestaurentCard from "./RestaurentCard";
 import restaurantList  from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body=()=>{
     const [resList,setResList]=useState(restaurantList);
+
+    // useEffect(()=>{
+    //     console.log("useeffect called")
+    // },[])
+    useEffect(()=>{
+        fetchData();
+         
+    },[])
+//fetch return promise
+    const fetchData= async ()=>{
+     
+        const data= await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json,"sa")
+    }
+
+  
+
     return(
         <div className="body">
             <div className="filter">
